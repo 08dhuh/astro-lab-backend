8c14f38dabf4d5a5789c96f6e331b056d770f70c
Doran Huh <08dhuh@gmail.com>
2025-03-21 10:10:34 +1100

    Added corsmiddleware setting to allow frontend api requests

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
+#import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
+#from dotenv import load_dotenv
 from app.main_routes import router
 from app.utils.db_utils import is_database_ready
+
 
 
 @asynccontextmanager
@@ -11,6 +15,16 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+# load_dotenv()
+# origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 
 app.include_router(router)
